1fa7cd166436d7f84953838e221fde0ebd0e67f6
Rex Isaac Raphael <rex.raphael@outlook.com>
2020-10-05 19:19:00 +0200

    [WIP] implementing the evaluators

--- src/evaluators/value.rs
+++ src/evaluators/value.rs
@@ -1,8 +1,41 @@
-fn get_type() {
+#[derive(Debug)]
+enum GroqValueName {
+    Null,
+    Boolean,
+    Number,
+    Array,
+    Object,
+    Range,
+    Pair,
+    Path,
+}
+
+fn get_type() -> GroqValueName {
 
 }
 
+#[derive(Debug)]
+struct StaticValue<P> {
+    data: P
+}
 
-struct StaticValue {
-    data: i8
+impl StaticValue<P> {
+
+    fn new(&self, new_data: P) {
+        self.data = new_data
+    }
+
+    fn get_data(&self) -> P {
+        self.data
+    }
+
+    fn get_boolean() -> bool {
+        // TODO run type check on data
+        true
+    }
+}
+
+#[derive(Debug)]
+struct StreamValue {
+    data: Vec<i8>
 }
